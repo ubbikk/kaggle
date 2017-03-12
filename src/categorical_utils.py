cb04d408061aefc15ccce2a4ed60af9155a92db9
ubik <dd.petrovskiy@gmail.com>
2017-03-12 18:36:35 +0200

    working

--- src/categorical_utils.py
+++ src/categorical_utils.py
@@ -1,6 +1,9 @@
 import pandas as pd
 from collections import OrderedDict
 import math
+import seaborn as sns
+sns.set(color_codes=True)
+sns.set(style="whitegrid", color_codes=True)
 
 
 def cols_forM(col, target_col, target_vals, m):
@@ -19,19 +22,14 @@ def process_with_lambda(train_df, test_df, col, target_col, target_vals, lambda_
     priors = train_df[dummies_cols].mean()
     priors_arr = [priors[dummy_col(target_col, v)] for v in target_vals]
     agg = OrderedDict(
-        [(dummy_col(target_col, v), OrderedDict([('{}_mean'.format(v),'mean'),('{}_count'.format(v),'count')])) for v in target_vals]
+        [(dummy_col(target_col, v), OrderedDict([('{}_mean'.format(v),'mean')])) for v in target_vals] + [(col, {'cnt':'count'})]
     )
     df = train_df[[col]+dummies_cols].groupby(col).agg(agg)
-    cols_blja = []
-    for v in target_vals:
-        cols_blja.append('posterior_{}'.format(v))
-        cols_blja.append('count_{}'.format(v))
-    df.columns = cols_blja
+    df.columns = ['posterior_{}'.format(v) for v in target_vals] + ['cnt']
     new_cols=[]
     for v in target_vals:
         def norm_posterior(x):
-            cnt = sum([x['count_{}'.format(yy)] for yy in target_vals])
-            cnt= float(cnt)
+            cnt= float(x['cnt'])
             posterior = x['posterior_{}'.format(v)]
             prior = priors[dummy_col(target_col, v)]
             l = lambda_f(cnt)
@@ -58,50 +56,8 @@ def process_with_lambda(train_df, test_df, col, target_col, target_vals, lambda_
 
 
 def processM(train_df, test_df, col, target_col, target_vals, m):
-    m = float(m)
-    temp_target = '{}_'.format(target_col)
-    train_df[temp_target]= train_df[target_col]
-    train_df= pd.get_dummies(train_df, columns=[target_col])
-    dummies_cols = [dummy_col(target_col, v) for v in target_vals]
-    priors = train_df[dummies_cols].mean()
-    priors_arr = [priors[dummy_col(target_col, v)] for v in target_vals]
-    agg = OrderedDict(
-        [(dummy_col(target_col, v), OrderedDict([('{}_mean'.format(v),'mean'),('{}_count'.format(v),'count')])) for v in target_vals]
-    )
-    df = train_df[[col]+dummies_cols].groupby(col).agg(agg)
-    cols_blja = []
-    for v in target_vals:
-        cols_blja.append('posterior_{}'.format(v))
-        cols_blja.append('count_{}'.format(v))
-    df.columns = cols_blja
-    new_cols=[]
-    for v in target_vals:
-        def norm_posterior(x):
-            cnt = sum([x['count_{}'.format(yy)] for yy in target_vals])
-            cnt= float(cnt)
-            posterior = x['posterior_{}'.format(v)]
-            prior = priors[dummy_col(target_col, v)]
-            l = cnt/(cnt+m)
-            return (l * posterior) + ((1 - l) * prior)
-
-        new_col = '{}_catM={}_for_{}={}'.format(col, m, target_col, v)
-        df[new_col] =df.apply(norm_posterior, axis=1)
-        new_cols.append(new_col)
-
-    df = df[new_cols]
-
-    train_df = pd.merge(train_df, df, left_on=col, right_index=True)
-
-    test_df = pd.merge(test_df, df, left_on=col, right_index=True, how='left')
-    test_df.loc[test_df[new_cols[0]].isnull(), new_cols] = priors_arr
-
-    for c in dummies_cols:
-        del train_df[c]
-
-    train_df[target_col]= train_df[temp_target]
-    del train_df[temp_target]
-
-    return train_df, test_df
+    lambda_f = lambda cnt: cnt/(cnt+m)
+    return process_with_lambda(train_df, test_df, col, target_col, target_vals, lambda_f)
 
 def dummy_col(col_name, val):
     return '{}_{}'.format(col_name, val)
@@ -112,18 +68,84 @@ def get_exp_lambda(k,f):
         return 1/(1+math.exp(float(k-n)/f))
     return res
 
-def visualize_exp_lambda():
+
+def visualize_exp_lambda(k,f):
     import matplotlib.pyplot as plt
+    plt.interactive(False)
+
     fig, ax = plt.subplots()
     sz=300
     ixes = range(sz)
-    l0=get_exp_lambda(50, 1)
-    l1=get_exp_lambda(50, 5)
-    l2=get_exp_lambda(50, 0.2)
+    l0=get_exp_lambda(k, f)
     to_plot0 = [l0(x) for x in ixes]
-    to_plot1 = [l1(x) for x in ixes]
-    to_plot2 = [l2(x) for x in ixes]
-    ax.plot(ixes, to_plot0, label='f=1')
-    ax.plot(ixes, to_plot1, label='f=5')
-    ax.plot(ixes, to_plot2, label='f=0.2')
+    ax.plot(ixes, to_plot0, label='')
     ax.legend()
+    plt.show()
+
+
+def create_count_label_column(df, count_col, new_col, cutoffs):
+    def count_lbl(x):
+        if x<=cutoffs[0]:
+            return '(0, {}]'.format(cutoffs[0])
+        sz = len(cutoffs)
+        for i in range(sz -1):
+            start = cutoffs[i]
+            end = cutoffs[i+1]
+            if x>start and x<=end:
+                if end-start>1:
+                    return '({}, {}]'.format(start, end)
+                else:
+                    return str(end)
+
+        last = cutoffs[sz - 1]
+        if x> last:
+            return '({}, inf)'.format(last)
+
+        raise Exception()
+
+    df[new_col]=df[count_col].apply(count_lbl)
+    return df
+
+def cutoffs_labels(cutoffs):
+    first = cutoffs[0]
+    sz=len(cutoffs)
+    last=cutoffs[sz-1]
+    res = []
+    res.append('(0, {}]'.format(first))
+    for i in range(sz-1):
+        start = cutoffs[i]
+        end = cutoffs[i+1]
+        if end-start>1:
+            res.append('({}, {}]'.format(start, end))
+        else:
+            res.append(str(end))
+
+    res.append('({}, inf)'.format(last))
+
+    return res
+
+
+def explore_value_counts(df, col_name, cutoffs):
+    tmp=df[col_name].to_frame(col_name)
+    tmp['count_col'] = df.groupby(col_name)[col_name].transform('count')
+    tmp = create_count_label_column(tmp, 'count_col', 'count_label', cutoffs)
+    sns.factorplot(x='count_label', data=tmp, kind='count', order=cutoffs_labels(cutoffs))
+
+
+
+# def visualize_exp_lambda():
+#     import matplotlib.pyplot as plt
+#
+#     fig, ax = plt.subplots()
+#     sz=300
+#     ixes = range(sz)
+#     l0=get_exp_lambda(50, 1)
+#     l1=get_exp_lambda(50, 5)
+#     l2=get_exp_lambda(50, 0.2)
+#     to_plot0 = [l0(x) for x in ixes]
+#     to_plot1 = [l1(x) for x in ixes]
+#     to_plot2 = [l2(x) for x in ixes]
+#     ax.plot(ixes, to_plot0, label='f=1')
+#     ax.plot(ixes, to_plot1, label='f=5')
+#     ax.plot(ixes, to_plot2, label='f=0.2')
+#     ax.legend()
--- src/manager_id_25_no_skill.py
+++ src/manager_id_25_no_skill.py
@@ -3,6 +3,7 @@ import os
 import seaborn as sns
 import pandas as pd
 from collections import OrderedDict
+from time import time
 
 from matplotlib import pyplot
 from scipy.sparse import coo_matrix
@@ -15,30 +16,26 @@ from sklearn.metrics import log_loss
 from xgboost import plot_importance
 from sklearn.model_selection import train_test_split
 from scipy.stats import boxcox
-from scipy.spatial import KDTree
 
 src_folder = '/home/ubik/PycharmProjects/kaggle/src'
 os.chdir(src_folder)
 import sys
-
 sys.path.append(src_folder)
 
-from categorical_utils import process_with_lambda, cols, get_exp_lambda, visualize_exp_lambda
 from v2w import avg_vector_df, load_model, avg_vector_df_and_pca
 
 TARGET = u'interest_level'
-TARGET_VALUES = ['low', 'medium', 'high']
 MANAGER_ID = 'manager_id'
 BUILDING_ID = 'building_id'
-LATITUDE = 'latitude'
-LONGITUDE = 'longitude'
-PRICE = 'price'
-BATHROOMS = 'bathrooms'
-BEDROOMS = 'bedrooms'
-DESCRIPTION = 'description'
-DISPLAY_ADDRESS = 'display_address'
-STREET_ADDRESS = 'street_address'
-LISTING_ID = 'listing_id'
+LATITUDE='latitude'
+LONGITUDE='longitude'
+PRICE='price'
+BATHROOMS='bathrooms'
+BEDROOMS='bedrooms'
+DESCRIPTION='description'
+DISPLAY_ADDRESS='display_address'
+STREET_ADDRESS='street_address'
+LISTING_ID='listing_id'
 PRICE_PER_BEDROOM = 'price_per_bedroom'
 
 FEATURES = [u'bathrooms', u'bedrooms', u'building_id', u'created',
@@ -46,10 +43,10 @@ FEATURES = [u'bathrooms', u'bedrooms', u'building_id', u'created',
             u'latitude', u'listing_id', u'longitude', MANAGER_ID, u'photos',
             u'price', u'street_address']
 
-sns.set(color_codes=True)
-sns.set(style="whitegrid", color_codes=True)
+# sns.set(color_codes=True)
+# sns.set(style="whitegrid", color_codes=True)
 
-pd.set_option('display.max_columns', 500)
+# pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 1000)
 pd.set_option('display.max_rows', 500)
 
@@ -69,24 +66,6 @@ def load_train():
 def load_test():
     return basic_preprocess(pd.read_json(test_file))
 
-def process_outliers_lat_long(train_df, test_df):
-    min_lat=40
-    max_lat=41
-    min_long=-74.1
-    max_long=-73
-
-    good_lat = (train_df[LATITUDE] < max_lat) & (train_df[LATITUDE] > min_lat)
-    good_long = (train_df[LONGITUDE] < max_long) & (train_df[LONGITUDE] > min_long)
-
-    train_df = train_df[good_lat & good_long]
-
-    bed_lat = (test_df[LATITUDE] >=max_lat) | (test_df[LATITUDE] <=min_lat)
-    bed_long = (test_df[LONGITUDE] >= max_long) | (test_df[LONGITUDE] <= min_long)
-    test_df[LATITUDE][bed_lat] = train_df[LATITUDE].mean()
-    test_df[LONGITUDE][bed_long]=train_df[LONGITUDE].mean()
-
-    return train_df, test_df
-
 
 def basic_preprocess(df):
     df['num_features'] = df[u'features'].apply(len)
@@ -102,23 +81,25 @@ def basic_preprocess(df):
     return df
 
 
-# (0.61509489625789615, [0.61124170916042475, 0.61371758902339113, 0.61794752159334343, 0.61555861194203254, 0.61700904957028924])
-def with_lambda_loss(df, k, f):
+
+
+def mngr_no_skill_loss(df):
     features = ['bathrooms', 'bedrooms', 'latitude', 'longitude', 'price',
                 'num_features', 'num_photos', 'word_num_in_descr',
-                "created_year", "created_month", "created_day"]+ \
-               cols(MANAGER_ID, TARGET, TARGET_VALUES)
+                "created_month", "created_day"]
+    mngr_features_only = ['man_id_high', 'man_id_medium', 'man_id_low']
+
 
     train_df, test_df = split_df(df, 0.7)
-    lamdba_f = get_exp_lambda(k, f)
-    train_df, test_df = process_with_lambda(train_df, test_df, MANAGER_ID, TARGET, TARGET_VALUES, lamdba_f)
+    # train_df, test_df = process_price_per_bedroom(train_df, test_df)
+    train_df, test_df = process_manager_id(train_df, test_df)
 
     train_target, test_target = train_df[TARGET].values, test_df[TARGET].values
     del train_df[TARGET]
     del test_df[TARGET]
 
-    train_df = train_df[features]
-    test_df = test_df[features]
+    train_df = train_df[features+mngr_features_only]
+    test_df = test_df[features+mngr_features_only]
 
     train_arr, test_arr = train_df.values, test_df.values
 
@@ -136,44 +117,54 @@ def with_lambda_loss(df, k, f):
     proba = estimator.predict_proba(test_arr)
     return log_loss(test_target, proba)
 
-
-def do_test_k_f(k, f, fp):
-    visualize_exp_lambda(k,f)
-    l=[]
-    for t in range(200):
-        loss = with_lambda_loss(load_train(), k,f)
-        l.append(loss)
+def do_test(num, fp):
+    neww = []
+    train_df = load_train()
+    for x in range(num):
+        t = time()
+        df=train_df.copy()
+        loss = mngr_no_skill_loss(df)
         print loss
-        with open(fp, 'w+') as fl:
-            json.dump(l, fl)
+        print 'time: {}'.format(time()-t)
+        print
+        neww.append(loss)
+        with open(fp, 'w+') as f:
+            json.dump(neww, f)
 
-    print
-    print 'avg={}'.format(np.mean(l))
-    print '\n\n'
+    print '\n\n\n\n'
+    print 'avg = {}'.format(np.mean(neww))
+
+def process_manager_id(train_df, test_df):
+    cutoff = 25
+    """@type train_df: pd.DataFrame"""
+    df = train_df[[MANAGER_ID, TARGET]]
+    df = pd.get_dummies(df, columns=[TARGET])
+    agg = OrderedDict([
+        (MANAGER_ID, {'count': 'count'}),
+        ('interest_level_high', {'high': 'mean'}),
+        ('interest_level_medium', {'medium': 'mean'}),
+        ('interest_level_low', {'low': 'mean'})
+    ])
+    df = df.groupby(MANAGER_ID).agg(agg)
+
+    df.columns = ['man_id_count', 'man_id_high', 'man_id_medium', 'man_id_low']
+
+    big = df['man_id_count'] >= cutoff
+    small = ~big
+    bl = df[['man_id_high', 'man_id_medium', 'man_id_low']][big].mean()
+    df.loc[small, ['man_id_high', 'man_id_medium', 'man_id_low']] = bl.values
+
+    df = df[['man_id_high', 'man_id_medium', 'man_id_low']]
+    train_df = pd.merge(train_df, df, left_on=MANAGER_ID, right_index=True)
+
+    test_df = pd.merge(test_df, df, left_on=MANAGER_ID, right_index=True, how='left')
+    test_df.loc[test_df['man_id_high'].isnull(), ['man_id_high', 'man_id_medium', 'man_id_low']] = bl.values
+
+    train_df['manager_skill'] = train_df['man_id_high'] * 2 + train_df['man_id_medium']
+    test_df['manager_skill'] = test_df['man_id_high'] * 2 + test_df['man_id_medium']
+
+    return train_df, test_df
 
 
 
-def do_test():
-    for k in (20, 10, 5, 2, 30, 50):
-        for f in (5.0, 1.0, 2.5, 10, 0.5):
-            print 'k={}, f={}'.format(k,f)
-            fp='/home/ubik/PycharmProjects/kaggle/trash/with_lambda/k={}_f={}.json'.format(k,f)
-            l=[]
-            for t in range(200):
-                loss = with_lambda_loss(load_train(), k,f)
-                l.append(loss)
-
-                with open(fp, 'w+') as fl:
-                    json.dump(l, fl)
-
-                print loss
-
-
-            print
-            print 'avg={}'.format(np.mean(l))
-            print '\n\n'
-
-
-
-
-do_test_k_f(25, 0.1, '')
+do_test(200, '/home/ubik/PycharmProjects/kaggle/trash/manager_id_25_no_skill.json')
