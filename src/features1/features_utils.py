08b84b8ce6a4e6e93c930d87e486f268794bebb7
ubik <dd.petrovskiy@gmail.com>
2017-03-18 17:56:28 +0200

    working

--- src/features1/features_utils.py
+++ src/features1/features_utils.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from nltk.tokenize import TweetTokenizer
 
 F_COL=u'features'
 
@@ -24,8 +25,11 @@ def get_c_map_ordered(s):
 
     return c_map
 
-def get_top_N_counts(s,N):
-    return get_c_map_ordered(s)[:N]
+def get_top_N_counts(s,N=None):
+    c_map_ordered = get_c_map_ordered(s)
+    if N is None:
+        N = len(c_map_ordered)
+    return c_map_ordered[:N]
 
 def get_top_N_features(s,N):
     return [x[0] for x in get_top_N_counts(s, N)]
@@ -62,3 +66,30 @@ def add_features_list(df, l):
 
     return df
 
+
+def desc_to_file(df, N, fp):
+    N = len(df) if N is None else N
+    df = df[:N]
+    with open(fp, 'w+') as f:
+        for s in df['description']:
+            f.write(s.encode('utf-8'))
+            f.write('\n\n\n\n')
+
+
+def write_tokens_counts(df, fp):
+    lower_df(df)
+    tokenizer = TweetTokenizer()
+    c_map={}
+    for l in df[F_COL]:
+        for f in l:
+            for t in tokenizer.tokenize(f):
+                if t in c_map:
+                    c_map[t]+=1
+                else:
+                    c_map[t]=1
+
+
+    c_map=[(k,v) for k,v in c_map.iteritems()]
+    c_map.sort(key=lambda s:s[1], reverse=True)
+    with open(fp, 'w+') as f:
+        f.write('\n'.join([str(x) for x in c_map]))
