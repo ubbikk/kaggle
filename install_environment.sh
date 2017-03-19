13ea77bdea15b9653a6e9cd7d18324579693db90
ubik <dd.petrovskiy@gmail.com>
2017-03-19 17:51:26 +0200

    working

---
+++ install_environment.sh
@@ -0,0 +1,23 @@
+apt-get -y install git
+apt-get -y install gcc
+apt-get -y install make
+apt-get -y install g++
+apt-get -y install python-dev
+apt-get -y install python-tk
+
+wget https://bootstrap.pypa.io/get-pip.py
+python get-pip.py
+pip install scipy
+pip install pandas
+pip install -U scikit-learn
+pip install hyperopt
+pip install dill
+pip install seaborn
+
+
+git clone http://github.com/dmlc/xgboost
+cd xgboost
+git submodule update --init
+./build.sh
+cd python-package
+python setup.py install
