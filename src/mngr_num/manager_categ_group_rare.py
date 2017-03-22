eb46fa7b6b592fe4197fb3f5a2458fa254bfb9d8
dpetrovskyi <dmytro.petrovskyi@zorallabs.com>
2017-03-22 16:05:13 +0200

    working

--- src/mngr_num/manager_categ_group_rare.py
+++ src/mngr_num/manager_categ_group_rare.py
@@ -14,11 +14,12 @@ from sklearn.cross_validation import cross_val_score, KFold
 import numpy as np
 import xgboost as xgb
 from sklearn.metrics import log_loss
+from sklearn.preprocessing import LabelEncoder
 from xgboost import plot_importance
 from sklearn.model_selection import train_test_split
 from scipy.stats import boxcox
 from scipy.spatial import KDTree
-from sklearn.preprocessing import LabelEncoder
+import math
 
 
 
@@ -37,6 +38,10 @@ STREET_ADDRESS = 'street_address'
 LISTING_ID = 'listing_id'
 PRICE_PER_BEDROOM = 'price_per_bedroom'
 F_COL=u'features'
+CREATED_MONTH = "created_month"
+CREATED_DAY = "created_day"
+CREATED_MINUTE='created_minute'
+CREATED_HOUR = 'created_hour'
 
 FEATURES = [u'bathrooms', u'bedrooms', u'building_id', u'created',
             u'description', u'display_address', u'features',
@@ -53,21 +58,224 @@ pd.set_option('display.max_rows', 5000)
 train_file = '../../data/redhoop/train.json'
 test_file = '../../data/redhoop/test.json'
 
-# def process_manager_num(train_df, test_df):
-#     mngr_num_col = 'manager_num'
-#     df = train_df.groupby(MANAGER_ID)[MANAGER_ID].count()
-#     # df[df<=1]=-1
-#     df = df.apply(float)
-#     df = df.to_frame(mngr_num_col)
-#     train_df = pd.merge(train_df, df, left_on=MANAGER_ID, right_index=True)
-#     test_df = pd.merge(test_df, df, left_on=MANAGER_ID, right_index=True, how='left')
-#
-#     return train_df, test_df, [mngr_num_col]
 
-def process_manager_categ(train_df, test_df, num):
+def process_listing_id(train_df, test_df):
+    return train_df, test_df, [LISTING_ID]
+
+
+
+
+
+
+
+
+
+
+
+def cols(col, target_col, target_vals):
+    return ['{}_coverted_exp_for_{}={}'.format(col, target_col, v) for v in target_vals]
+
+def dummy_col(col_name, val):
+    return '{}_{}'.format(col_name, val)
+
+def process_with_lambda(train_df, test_df, col, target_col, target_vals, lambda_f):
+    temp_target = '{}_'.format(target_col)
+    train_df[temp_target]= train_df[target_col]
+    train_df= pd.get_dummies(train_df, columns=[target_col])
+    dummies_cols = [dummy_col(target_col, v) for v in target_vals]
+    priors = train_df[dummies_cols].mean()
+    priors_arr = [priors[dummy_col(target_col, v)] for v in target_vals]
+    agg = OrderedDict(
+        [(dummy_col(target_col, v), OrderedDict([('{}_mean'.format(v),'mean')])) for v in target_vals] + [(col, {'cnt':'count'})]
+    )
+    df = train_df[[col]+dummies_cols].groupby(col).agg(agg)
+    df.columns = ['posterior_{}'.format(v) for v in target_vals] + ['cnt']
+    new_cols=[]
+    for v in target_vals:
+        def norm_posterior(x):
+            cnt= float(x['cnt'])
+            posterior = x['posterior_{}'.format(v)]
+            prior = priors[dummy_col(target_col, v)]
+            l = lambda_f(cnt)
+            return (l * posterior) + ((1 - l) * prior)
+
+        new_col = '{}_coverted_exp_for_{}={}'.format(col, target_col, v)
+        df[new_col] =df.apply(norm_posterior, axis=1)
+        new_cols.append(new_col)
+
+    df = df[new_cols]
+
+    train_df = pd.merge(train_df, df, left_on=col, right_index=True)
+
+    test_df = pd.merge(test_df, df, left_on=col, right_index=True, how='left')
+    test_df.loc[test_df[new_cols[0]].isnull(), new_cols] = priors_arr
+
+    for c in dummies_cols:
+        del train_df[c]
+
+    train_df[target_col]= train_df[temp_target]
+    del train_df[temp_target]
+
+    return train_df, test_df, new_cols
+
+
+def get_exp_lambda(k,f):
+    def res(n):
+        return 1/(1+math.exp(float(k-n)/f))
+    return res
+
+
+def process_mngr_categ_preprocessing(train_df, test_df):
+    col = MANAGER_ID
+    k=15.0
+    f=0.14119444578
+    lamdba_f = get_exp_lambda(k, f)
+    return process_with_lambda(train_df, test_df, col, TARGET, TARGET_VALUES, lamdba_f)
+
+
+def process_bid_categ_preprocessing(train_df, test_df):
+    col = BUILDING_ID
+    k=51.0
+    f=0.156103119211
+    lamdba_f = get_exp_lambda(k, f)
+    return process_with_lambda(train_df, test_df, col, TARGET, TARGET_VALUES, lamdba_f)
+
+
+def process_manager_num(train_df, test_df):
+    mngr_num_col = 'manager_num'
+    df = train_df.groupby(MANAGER_ID)[MANAGER_ID].count()
+    # df[df<=1]=-1
+    df = df.apply(float)
+    df = df.to_frame(mngr_num_col)
+    train_df = pd.merge(train_df, df, left_on=MANAGER_ID, right_index=True)
+    test_df = pd.merge(test_df, df, left_on=MANAGER_ID, right_index=True, how='left')
+
+    return train_df, test_df, [mngr_num_col]
+
+
+def process_bid_num(train_df, test_df):
+    bid_num_col = 'bid_num'
+    df = train_df.groupby(BUILDING_ID)[BUILDING_ID].count()
+    # df[df<=1]=-1
+    df = df.apply(float)
+    df = df.to_frame(bid_num_col)
+    train_df = pd.merge(train_df, df, left_on=BUILDING_ID, right_index=True)
+    test_df = pd.merge(test_df, df, left_on=BUILDING_ID, right_index=True, how='left')
+
+    return train_df, test_df, [bid_num_col]
+
+
+
+
+
+
+
+
+COL = 'normalized_features'
+
+
+def normalize_df(df):
+    df[COL] = df[F_COL].apply(lambda l: [x.lower() for x in l])
+
+
+def lambda_in(in_arr):
+    def is_in(l):
+        for f in l:
+            for t in in_arr:
+                if t in f:
+                    return 1
+
+        return 0
+
+    return is_in
+
+
+def lambda_equal(val):
+    def is_equal(l):
+        for f in l:
+            if f.strip() == val:
+                return 1
+
+        return 0
+
+    return is_equal
+
+
+def lambda_two_arr(arr1, arr2):
+    def is_in(l):
+        for f in l:
+            for x in arr1:
+                for y in arr2:
+                    if x in f and y in f:
+                        return 1
+        return 0
+
+    return is_in
+
+
+GROUPING_MAP=OrderedDict(
+    [('elevator', {'vals': ['elevator'], 'type': 'in'}),
+     ('hardwood floors', {'vals': ['hardwood'], 'type': 'in'}),
+     ('cats allowed', {'vals': ['cats'], 'type': 'in'}),
+     ('dogs allowed', {'vals': ['dogs'], 'type': 'in'}),
+     ('doorman', {'vals': ['doorman', 'concierge'], 'type': 'in'}),
+     ('dishwasher', {'vals': ['dishwasher'], 'type': 'in'}),
+     ('laundry in building', {'vals': ['laundry'], 'type': 'in'}),
+     ('no fee', {'vals': ['no fee', 'no broker fee', 'no realtor fee'], 'type': 'in'}),
+     ('reduced fee', {'vals': ['reduced fee', 'reduced-fee', 'reducedfee'], 'type': 'in'}),
+     ('fitness center', {'vals': ['fitness'], 'type': 'in'}),
+     ('pre-war', {'vals': ['pre-war', 'prewar'], 'type': 'in'}),
+     ('roof deck', {'vals': ['roof'], 'type': 'in'}),
+     ('outdoor space',{'vals': ['outdoor space', 'outdoor-space', 'outdoor areas', 'outdoor entertainment'], 'type': 'in'}),
+     ('common outdoor space',{'vals': ['common outdoor', 'publicoutdoor', 'public-outdoor', 'common-outdoor'], 'type': 'in'}),
+     ('private outdoor space', {'vals': ['private outdoor', 'private-outdoor', 'privateoutdoor'], 'type': 'in'}),
+     ('dining room', {'vals': ['dining'], 'type': 'in'}),
+     ('high speed internet', {'vals': ['internet'], 'type': 'in'}),
+     ('balcony', {'vals': ['balcony'], 'type': 'in'}),
+     ('swimming pool', {'vals': ['swimming', 'pool'], 'type': 'in'}),
+     ('new construction', {'vals': ['new construction'], 'type': 'in'}),
+     ('terrace', {'vals': ['terrace'], 'type': 'in'}),
+     ('exclusive', {'vals': ['exclusive'], 'type': 'equal'}),
+     ('loft', {'vals': ['loft'], 'type': 'in'}),
+     ('garden/patio', {'vals': ['garden'], 'type': 'in'}),
+     ('wheelchair access', {'vals': ['wheelchair'], 'type': 'in'}),
+     ('fireplace', {'vals': ['fireplace'], 'type': 'in'}),
+     ('simplex', {'vals': ['simplex'], 'type': 'in'}),
+     ('lowrise', {'vals': ['lowrise', 'low-rise'], 'type': 'in'}),
+     ('garage', {'vals': ['garage'], 'type': 'in'}),
+     ('furnished', {'vals': ['furnished'], 'type': 'equal'}),
+     ('multi-level', {'vals': ['multi-level', 'multi level', 'multilevel'], 'type': 'in'}),
+     ('high ceilings', {'vals': ['high ceilings', 'highceilings', 'high-ceilings'], 'type': 'in'}),
+     ('parking space', {'vals': ['parking'], 'type': 'in'}),
+     ('live in super', {'vals': ['super'], 'vals2': ['live', 'site'], 'type': 'two'}),
+     ('renovated', {'vals': ['renovated'], 'type': 'in'}),
+     ('green building', {'vals': ['green building'], 'type': 'in'}),
+     ('storage', {'vals': ['storage'], 'type': 'in'}),
+     ('washer', {'vals': ['washer'], 'type': 'in'}),
+     ('stainless steel appliances', {'vals': ['stainless'], 'type': 'in'})])
+
+
+def process_features(df):
+    normalize_df(df)
+    new_cols=[]
+    for col, m in GROUPING_MAP.iteritems():
+        new_cols.append(col)
+        tp = m['type']
+        if tp == 'in':
+            df[col] = df[COL].apply(lambda_in(m['vals']))
+        elif tp=='equal':
+            df[col] = df[COL].apply(lambda_equal(m['vals'][0]))
+        elif tp=='two':
+            df[col] = df[COL].apply(lambda_two_arr(m['vals'], m['vals2']))
+        else:
+            raise Exception()
+
+    return df, new_cols
+
+
+def process_manager_categ_rare(train_df, test_df, num):
     train_df['manager_num'] = train_df.groupby(MANAGER_ID)[MANAGER_ID].transform('count')
-    for i in range(num):
-        train_df.loc[train_df['manager_num']==i, MANAGER_ID]='rare_manager_count={}'.format(i)
+    train_df.loc[train_df['manager_num']<=num, MANAGER_ID]='rare'
 
     new_col = 'manager_label'
     le = LabelEncoder()
@@ -79,6 +287,20 @@ def process_manager_categ(train_df, test_df, num):
     test_df.loc[~bad_managers, new_col] = le.transform(test_df.loc[~bad_managers, MANAGER_ID])
 
     return train_df, test_df, [new_col]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 def out(l, loss, l_1K, loss1K, num, t):
@@ -135,57 +357,45 @@ def basic_preprocess(df):
     df['num_photos'] = df['photos'].apply(len)
     df['word_num_in_descr'] = df['description'].apply(lambda x: len(x.split(' ')))
     df["created"] = pd.to_datetime(df["created"])
-    df["created_year"] = df["created"].dt.year
-    df["created_month"] = df["created"].dt.month
-    df["created_day"] = df["created"].dt.day
+    # df["created_year"] = df["created"].dt.year
+    df[CREATED_MONTH] = df["created"].dt.month
+    df[CREATED_DAY] = df["created"].dt.day
+    df[CREATED_HOUR] = df["created"].dt.hour
+    df[CREATED_MINUTE] = df["created"].dt.minute
     bc_price, tmp = boxcox(df['price'])
     df['bc_price'] = bc_price
 
     return df
 
 
-# (0.61509489625789615, [0.61124170916042475, 0.61371758902339113, 0.61794752159334343, 0.61555861194203254, 0.61700904957028924])
-def simple_loss(df):
-    features = ['bathrooms', 'bedrooms', 'latitude', 'longitude', 'price',
-                'num_features', 'num_photos', 'word_num_in_descr',
-                "created_month", "created_day"]
-
-    train_df, test_df = split_df(df, 0.7)
-
-    train_target, test_target = train_df[TARGET].values, test_df[TARGET].values
-    del train_df[TARGET]
-    del test_df[TARGET]
-
-    train_df = train_df[features]
-    test_df = test_df[features]
-
-    train_arr, test_arr = train_df.values, test_df.values
-
-    estimator = xgb.XGBClassifier(n_estimators=1000, objective='mlogloss')
-    # estimator = RandomForestClassifier(n_estimators=1000)
-    estimator.fit(train_arr, train_target)
-
-    # plot feature importance
-    # ffs= features[:len(features)-1]+['man_id_high', 'man_id_medium', 'man_id_low', 'manager_skill']
-    # sns.barplot(ffs, [x for x in estimator.feature_importances_])
-    # sns.plt.show()
-
-
-    # print estimator.feature_importances_
-    proba = estimator.predict_proba(test_arr)
-    return log_loss(test_target, proba)
-
 def get_loss_at1K(estimator):
     results_on_test = estimator.evals_result()['validation_1']['mlogloss']
     return results_on_test[1000]
 
-def loss_with_per_tree_stats(df):
+def loss_with_per_tree_stats(df, new_cols):
     features = ['bathrooms', 'bedrooms', 'latitude', 'longitude', 'price',
                 'num_features', 'num_photos', 'word_num_in_descr',
-                "created_year", "created_month", "created_day"]
+                "created_month", "created_day", CREATED_HOUR, CREATED_MINUTE]
+    features+=new_cols
 
     train_df, test_df = split_df(df, 0.7)
-    train_df, test_df, new_cols = process_manager_categ(train_df, test_df, 1)
+
+    train_df, test_df, new_cols = process_mngr_categ_preprocessing(train_df, test_df)
+    features+=new_cols
+
+    train_df, test_df, new_cols = process_manager_num(train_df, test_df)
+    features+=new_cols
+
+    train_df, test_df, new_cols = process_manager_categ_rare(train_df, test_df, 10)
+    features+=new_cols
+
+    train_df, test_df, new_cols = process_bid_categ_preprocessing(train_df, test_df)
+    features+=new_cols
+
+    train_df, test_df, new_cols = process_bid_num(train_df, test_df)
+    features+=new_cols
+
+    train_df, test_df, new_cols = process_listing_id(train_df, test_df)
     features+=new_cols
 
     train_target, test_target = train_df[TARGET].values, test_df[TARGET].values
@@ -196,9 +406,9 @@ def loss_with_per_tree_stats(df):
     test_df = test_df[features]
 
     train_arr, test_arr = train_df.values, test_df.values
+    print features
 
     estimator = xgb.XGBClassifier(n_estimators=1500, objective='mlogloss')
-    # estimator = RandomForestClassifier(n_estimators=1000)
     eval_set = [(train_arr, train_target), (test_arr, test_target)]
     estimator.fit(train_arr, train_target, eval_set=eval_set, eval_metric='mlogloss', verbose=False)
 
@@ -213,7 +423,7 @@ def loss_with_per_tree_stats(df):
 
     loss = log_loss(test_target, proba)
     loss1K = get_loss_at1K(estimator)
-    return loss, loss1K, xgboost_per_tree_results(estimator)
+    return loss, loss1K, xgboost_per_tree_results(estimator), estimator.feature_importances_
 
 def xgboost_per_tree_results(estimator):
     results_on_test = estimator.evals_result()['validation_1']['mlogloss']
@@ -223,31 +433,20 @@ def xgboost_per_tree_results(estimator):
         'test':results_on_test
     }
 
-
-def do_test(num, fp):
-    l = []
-    train_df = load_train()
-    for x in range(num):
-        t=time()
-        df=train_df.copy()
-
-        loss = simple_loss(df)
-        t=time()-t
-        l.append(loss)
-
-        out(l, loss,None,None, x, t)
-        write_results(l, fp)
-
 def do_test_with_xgboost_stats_per_tree(num, fp):
     l = []
     results =[]
     l_1K=[]
     train_df = load_train()
+    train_df, new_cols = process_features(train_df)
+    ii=[]
     for x in range(num):
         t=time()
         df=train_df.copy()
 
-        loss, loss1K, res = loss_with_per_tree_stats(df)
+        loss, loss1K, res , imp= loss_with_per_tree_stats(df, new_cols)
+        ii.append(imp.tolist())
+
         t=time()-t
         l.append(loss)
         l_1K.append(loss1K)
@@ -255,7 +454,7 @@ def do_test_with_xgboost_stats_per_tree(num, fp):
 
         out(l, loss, l_1K, loss1K, x, t)
         write_results(results, fp)
+        write_results(ii, 'importance.json')
 
 
-# train_df, test_df = load_train(), load_test()
-do_test_with_xgboost_stats_per_tree(1000, 'manager_categ_group_rare_only1.json')
+do_test_with_xgboost_stats_per_tree(1000, 'all_and_mngr_rare_10.json')
