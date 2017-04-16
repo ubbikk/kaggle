fe22551cbd707af9d0f5e710a5c428aeb3c254b0
ubik <dd.petrovskiy@gmail.com>
2017-04-16 19:02:13 +0300

    working

--- src/utils/mongo_ulils_new.py
+++ src/utils/mongo_ulils_new.py
@@ -8,39 +8,45 @@ from numpy import mean, std
 from pymongo import MongoClient
 from scipy.stats import normaltest
 import pandas as pd
+from sklearn.metrics import log_loss
 
 sns.set(color_codes=True)
 sns.set(style="whitegrid", color_codes=True)
 
-gc_host='35.187.46.132'
+gc_host = '35.187.46.132'
 local_host = '10.20.0.144'
 
 host = gc_host
 
 client = MongoClient(host, 27017)
-CV=5
+CV = 5
+TARGET = 'interest_level'
 
 
 def load_results(name):
-    db=client[name]
+    db = client[name]
     collection = db['losses']
     return [x['val'] for x in collection.find()]
 
+
 def load_results_1K(name):
-    db=client[name]
+    db = client[name]
     collection = db['losses']
     return [x['val']['test'][1000] for x in collection.find()]
 
+
 def load_results_atN(name, N):
-    db=client[name]
+    db = client[name]
     collection = db['losses']
     return [x['val']['test'][N] for x in collection.find()]
 
+
 def load_features_names(name):
-    db=client[name]
+    db = client[name]
     collection = db['features']
     for x in collection.find():
         return x['val']
+
 
 def load_importance(name):
     features = load_features_names(name)
@@ -54,34 +60,35 @@ def load_importance(name):
 
 
 def load_importance_raw(name):
-    db=client[name]
+    db = client[name]
     collection = db['importance']
     return [x['val'] for x in collection.find()]
+
 
 def explore_importance(name, N=None):
     features = load_features_names(name)
     if N is None:
-        N=len(features)
+        N = len(features)
 
     res = load_importance(name)
     print res
-    res=res[:N]
+    res = res[:N]
     xs = [x[0] for x in res]
-    ys=[x[1] for x in res]
+    ys = [x[1] for x in res]
     sns.barplot(xs, ys)
     sns.plt.show()
 
 
 def explore_res_name(name):
     res = load_results_1K(name)
-    s = std(res)/math.sqrt(len(res))
+    s = std(res) / math.sqrt(len(res))
     m = mean(res)
     return [
         ('avg_loss  ', m),
         ('max       ', max(res)),
         ('min       ', min(res)),
-        ('2s        ', '[{}, {}]'.format(m-2*s, m+2*s)),
-        ('3s        ', '[{}, {}]'.format(m-3*s, m+3*s)),
+        ('2s        ', '[{}, {}]'.format(m - 2 * s, m + 2 * s)),
+        ('3s        ', '[{}, {}]'.format(m - 3 * s, m + 3 * s)),
         ('norm      ', normaltest(res).pvalue),
         ('std       ', np.std(res)),
         ('mean_std  ', s),
@@ -91,11 +98,11 @@ def explore_res_name(name):
 
 def plot_errors_name(name):
     results = load_results(name)
-    train_runs= [x['train'] for x in results]
-    test_runs= [x['test'] for x in results]
+    train_runs = [x['train'] for x in results]
+    test_runs = [x['test'] for x in results]
 
-    sz=len(train_runs[0])
-    x_axis=range(sz)
+    sz = len(train_runs[0])
+    x_axis = range(sz)
     y_train = [np.mean([x[j] for x in train_runs]) for j in x_axis]
     y_test = [np.mean([x[j] for x in test_runs]) for j in x_axis]
 
@@ -106,18 +113,18 @@ def plot_errors_name(name):
 
 
 def get_probs(name, n=0):
-    probs=[]
-    ns=[CV*n + j for j in range(CV)]
-    db=client[name]
+    probs = []
+    ns = [CV * n + j for j in range(CV)]
+    db = client[name]
     collection = db['probs']
     for p in collection.find():
         N = p['N']
         if N in ns:
             probs.append(p)
-        if len(probs)==CV:
+        if len(probs) == CV:
             break
 
-    if len(probs)<CV:
+    if len(probs) < CV:
         raise
 
     return df_from_list(probs)
@@ -131,24 +138,85 @@ def df_from_list(probs):
     return pd.concat(dfs)[['low', 'medium', 'high']]
 
 
+def get_all_probs_very_raw(name):
+    db = client[name]
+    collection = db['probs']
+    return [p for p in collection.find()]
+
+
 def get_all_probs(name):
-    probs_map={}
-    db=client[name]
+    res = get_all_probs_raw(name)
+    return [df_from_list(p) for p in res]
+
+
+def get_all_probs_raw(name):
+    probs_map = {}
+    db = client[name]
     collection = db['probs']
     for p in collection.find():
         N = p['N']
         probs_map[N] = p
 
-    m = (1+max(probs_map.keys()))/CV
+    m = (1 + max(probs_map.keys())) / CV
     res = []
     for j in range(m):
-        probs =[]
-        for i in range(CV*j, CV*(j+1)):
+        probs = []
+        for i in range(CV * j, CV * (j + 1)):
             if i in probs_map:
                 probs.append(probs_map[i])
 
-        if len(probs)!=CV:
+        if len(probs) != CV:
             continue
-        res.append(df_from_list(probs))
+        res.append(probs)
 
     return res
+
+def df_form_probs_mg_entry(p):
+    return pd.DataFrame(p['val'], index=p['index'])[['high', 'low', 'medium']]
+
+def explore_stack_cv_error(probs_raw, train_df):
+    cv_dfs =[[] for _ in range(CV)]
+    indexes = [probs_raw[0][j]['index'] for j in range(CV)]
+    for cv in probs_raw:
+        for j in range(CV):
+            df = df_form_probs_mg_entry(cv[j])
+            cv_dfs[j].append(df)
+
+    cv_dfs = [sum(x)/len(x) for x in cv_dfs]
+
+    cv_loses = [log_loss(train_df[TARGET].loc[indexes[j]], cv_dfs[j].loc[indexes[j]].values) for j in range(CV)]
+
+    return {
+        'all':cv_loses,
+        'avg': np.mean(cv_loses)
+    }
+
+
+
+def explore_cv_errors(probs_raw, train_df):
+    cv_errors = []
+    errors_flat = []
+    for p in probs_raw:
+        errors = []
+        for f in p:
+            index = f['index']
+            target = train_df.loc[index][TARGET]
+            df = pd.DataFrame(f['val'])
+            probs = df[['high', 'low', 'medium']].values
+            loss = log_loss(target, probs)
+            errors.append(loss)
+            errors_flat.append(loss)
+
+        cv_errors.append(np.mean(errors))
+
+    return [
+        ('num', len(probs_raw)),
+        ('cv_mean', np.mean(cv_errors)),
+        ('cv_std', np.std(cv_errors)),
+        ('cv_max', np.max(cv_errors)),
+        ('cv_min', np.min(cv_errors)),
+
+        ('flat_mean', np.mean(errors_flat)),
+        ('flat_std', np.std(errors_flat)),
+        ('flat_max', np.max(errors_flat)),
+        ('flat_min', np.min(errors_flat)), ]
