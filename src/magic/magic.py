2e2ceb8f6ab17d943ea42389e3c5dffe4459732e
ubik <dd.petrovskiy@gmail.com>
2017-04-22 22:32:46 +0300

    working

--- src/magic/magic.py
+++ src/magic/magic.py
@@ -7,8 +7,9 @@ LISTING_ID = 'listing_id'
 
 def process_magic(train_df, test_df):
     image_date = pd.read_csv(magic_file)
+
     image_date.loc[80240,"time_stamp"] = 1478129766
-    # image_date.loc[image_date['Listing_Id']==7119094, "time_stamp"] = 1478129766
+
     image_date["img_date"] = pd.to_datetime(image_date["time_stamp"], unit="s")
     image_date["img_days_passed"] = (image_date["img_date"].max() - image_date["img_date"]).astype(
         "timedelta64[D]").astype(int)
@@ -24,14 +25,18 @@ def process_magic(train_df, test_df):
         lambda x: 1 if x < 10 else 2 if x < 20 else 3)
 
     df = pd.concat([train_df, test_df])
-    df = pd.merge(df, image_date, left_on=LISTING_ID, right_on='Listing_Id')
+    df = pd.merge(df, image_date,   left_on=LISTING_ID, right_on='Listing_Id')
     new_cols = ["img_days_passed","img_date_month","img_date_week",
                 "img_date_day","img_date_dayofweek","img_date_dayofyear",
                 "img_date_hour", "img_date_monthBeginMidEnd",
                 "img_date_minute", "img_date_second"]#+["img_date", "time_stamp"]
 
-    for col in new_cols:
-        train_df[col] = df.loc[train_df.index, col]
-        test_df[col] = df.loc[test_df.index, col]
+    # for col in new_cols:
+    #     train_df[col] = df.loc[train_df.index, col]
+    #     test_df[col] = df.loc[test_df.index, col]
+
+    df_to_merge = df[[LISTING_ID] + new_cols]
+    train_df = pd.merge(train_df, df_to_merge, on=LISTING_ID)
+    test_df = pd.merge(test_df, df_to_merge, on=LISTING_ID)
 
     return train_df, test_df, new_cols
