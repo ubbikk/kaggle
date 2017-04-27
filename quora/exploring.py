0ac999c3439451ce592b1e1624d7f9a5dc7c9403
dpetrovskyi <dmytro.petrovskyi@zorallabs.com>
2017-04-27 21:19:27 +0300

    w

---
+++ quora/exploring.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+
+sns.set(color_codes=True)
+sns.set(style="whitegrid", color_codes=True)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 1000)
+pd.set_option('display.max_rows', 5000)
+pd.set_option('display.max_colwidth', 100)
+
+
+fp_train= '../data/quora/train.csv'
+fp_test= '../data/quora/test.csv'
+
+qid1,  qid2 = 'qid1',  'qid2'
+
+
+def load_train_test():
+    return pd.read_csv(fp_train), pd.read_csv(fp_test)
+
+train_df, test_df = load_train_test()
---
+++ quora/keras_tests.py
@@ -0,0 +1,10 @@
+from keras.models import Sequential
+from keras.layers import Dense
+import numpy as np
+
+
+# load pima indians dataset
+dataset = np.loadtxt("pima-indians-diabetes.csv", delimiter=",")
+# split into input (X) and output (Y) variables
+X = dataset[:,0:8]
+Y = dataset[:,8]
