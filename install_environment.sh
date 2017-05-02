3759764a4f426bb110cfa5e1b13351fe7ab2b55e
ubik <dd.petrovskiy@gmail.com>
2017-05-02 08:17:31 +0300

    working

--- install_environment.sh
+++ install_environment.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 apt-get -y install git
 apt-get -y install gcc
 apt-get -y install make
@@ -7,6 +9,17 @@ apt-get -y install python-tk
 apt-get -y install unzip
 apt-get -y install htop
 apt-get -y install nmap
+apt-get -y install libcupti-dev
+
+echo "Checking for CUDA and installing."
+# Check for CUDA and try to install.
+if ! dpkg-query -W cuda; then
+  curl -O http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/cuda-repo-ubuntu1404_8.0.61-1_amd64.deb
+  dpkg -i ./cuda-repo-ubuntu1404_8.0.61-1_amd64.deb
+  apt-get update
+  apt-get install cuda -y
+  apt-get install linux-headers-$(uname -r) -y
+fi
 
 wget https://bootstrap.pypa.io/get-pip.py
 python get-pip.py
@@ -16,6 +29,9 @@ pip install -U scikit-learn
 pip install hyperopt
 pip install dill
 pip install seaborn
+pip install matplotlib
+pip install gensim
+pip install tensorflow
 
 
 git clone http://github.com/dmlc/xgboost
