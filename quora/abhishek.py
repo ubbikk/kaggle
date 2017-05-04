ac6b44897c4280b31fb64f4568d85e5b630f53aa
dpetrovskyi <dmytro.petrovskyi@zorallabs.com>
2017-05-04 17:32:47 +0300

    w

--- quora/abhishek.py
+++ quora/abhishek.py
@@ -81,7 +81,10 @@ def perform_xgb_cv():
         train_arr, train_target = big[FEATURES], big[TARGET]
         test_arr, test_target = small[FEATURES], small[TARGET]
 
-        estimator = xgb.XGBClassifier(n_estimators=5000, subsample=0.8, colsample_bytree=0.8)
+        estimator = xgb.XGBClassifier(n_estimators=2000,
+                                      subsample=0.8,
+                                      colsample_bytree=0.8,
+                                      max_depth=4)
         eval_set = [(train_arr, train_target), (test_arr, test_target)]
         estimator.fit(train_arr, train_target, eval_set=eval_set, eval_metric='logloss', verbose=False)
 
