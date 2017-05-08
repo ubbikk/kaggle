36705bc80f94a4ad4758119af0eae95ab5780958
ubik <dd.petrovskiy@gmail.com>
2017-05-08 23:15:59 +0300

    working

--- quora/basic_features.py
+++ quora/basic_features.py
@@ -73,5 +73,83 @@ def generate_lens(df, fp):
     df.to_csv(fp, index_label='id')
 
 
-def generate_common_words(df, fp):
+def get_common_tokens_ratio(a,b):
+    a = set(a.split())
+    b= set(b.split())
 
+    join = set(a)
+    join.update(b)
+
+    inter = a.intersection(b)
+
+    return 1.0*len(inter)/len(join)
+
+def get_common_tokens_num(a,b):
+    a = set(a.split())
+    b= set(b.split())
+    inter = a.intersection(b)
+
+    return len(inter)
+
+def get_common_tokens_ratio_no_stops(a,b):
+    a = set(a.split())
+    b= set(b.split())
+
+    join = set(a)
+    join.update(b)
+    join = join.difference(stop_words_with_PRON)
+    join = len(join)
+    join = 1 if join==0 else join
+
+    inter = a.intersection(b)
+    inter = inter.difference(stop_words_with_PRON)
+    inter = len(inter)
+
+    return 1.0*inter/join
+
+def get_common_tokens_num_no_stops(a,b):
+    a = set(a.split())
+    b= set(b.split())
+    inter = a.intersection(b)
+    inter = inter.difference(stop_words_with_PRON)
+    inter = len(inter)
+
+    return inter
+
+def get_stop_words_num(a):
+    a=set(a.split())
+    a= a.difference(stop_words_with_PRON)
+    return len(a)
+
+def get_stop_words_ratio(a):
+    a=set(a.split())
+    l = len(a)
+    a= a.difference(stop_words_with_PRON)
+    if l==0:
+        return 0
+    return 1.0*len(a)/l
+
+def generate_common_words(df, fp):
+    df['common_lemmas_num']=df[[lemmas_q1, lemmas_q2]].apply(lambda s: get_common_tokens_num(s[lemmas_q1], s[lemmas_q2]), axis=1)
+    df['common_stems_num']=df[[stems_q1, stems_q2]].apply(lambda s: get_common_tokens_num(s[stems_q1], s[stems_q2]), axis=1)
+
+    df['common_lemmas_num_no_stops']=df[[lemmas_q1, lemmas_q2]].apply(lambda s: get_common_tokens_num_no_stops(s[lemmas_q1], s[lemmas_q2]), axis=1)
+    df['common_lemmas_ratio_no_stops']=df[[lemmas_q1, lemmas_q2]].apply(lambda s: get_common_tokens_ratio_no_stops(s[lemmas_q1], s[lemmas_q2]), axis=1)
+
+    df['common_lemmas_ratio']=df[[lemmas_q1, lemmas_q2]].apply(lambda s: get_common_tokens_ratio(s[lemmas_q1], s[lemmas_q2]), axis=1)
+    df['common_stems_ratio']=df[[stems_q1, stems_q2]].apply(lambda s: get_common_tokens_ratio(s[stems_q1], s[stems_q2]), axis=1)
+
+    df['stop_words_num_q1']=df[lemmas_q1].apply(get_stop_words_num)
+    df['stop_words_num_q2']=df[lemmas_q2].apply(get_stop_words_num)
+
+    df['stop_words_ratio_q1']=df[lemmas_q1].apply(get_stop_words_ratio)
+    df['stop_words_ratio_q2']=df[lemmas_q2].apply(get_stop_words_ratio)
+
+    new_cols = ['common_lemmas_num', 'common_stems_num', 'common_lemmas_num_no_stops',
+                'common_lemmas_ratio_no_stops','common_lemmas_ratio', 'common_stems_ratio',
+                'stop_words_num_q1','stop_words_num_q2',
+                'stop_words_ratio_q1','stop_words_ratio_q2'
+                ]
+
+    df = df[new_cols]
+    df.to_csv(fp, index_label='id')
--- quora/data.py
+++ quora/data.py
@@ -6,31 +6,44 @@ import re
 sns.set(color_codes=True)
 sns.set(style="whitegrid", color_codes=True)
 pd.set_option('display.max_columns', 500)
-pd.set_option('display.width', 1000)
+pd.set_option('display.width', 5000)
 pd.set_option('display.max_rows', 5000)
 pd.set_option('display.max_colwidth', 100)
 
+fp_train = '../../data/train.csv'
+fp_test = '../../data/test.csv'
 
-fp_train= '../../data/train.csv'
-fp_test= '../../data/test.csv'
+lemmas_train_fp = '../../data/train_lemmas.csv'
+tokens_train_fp = '../../data/train_tokens.csv'
+nlp_train_fp = '../../data/postag_ner_train.json'
 
-lemmas_train_fp='../../data/train_lemmas.csv'
-tokens_train_fp='../../data/train_tokens.csv'
-nlp_train_fp='../../data/postag_ner_train.json'
+stems_train_fp = '../../data/train_porter.csv'
+stems_test_fp = '../../data/test_porter.csv'
 
-stems_train_fp='../../data/train_porter.csv'
-stems_test_fp='../../data/test_porter.csv'
+normalized_train_fp = '../../data/train_normalized.csv'
+common_words_train_fp = '../../data/train_common_words.csv'
+length_train_fp = '../../data/train_length.csv'
 
-normalized_train_fp='../../data/train_normalized.csv'
-
+METRICS_FP = [
+    '../../data/train_metrics_bool_lemmas.csv',
+    '../../data/train_metrics_bool_stems.csv',
+    '../../data/train_metrics_bool_tokens.csv',
+    '../../data/train_metrics_fuzzy_lemmas.csv',
+    '../../data/train_metrics_fuzzy_stems.csv',
+    '../../data/train_metrics_fuzzy_tokens.csv',
+    '../../data/train_metrics_sequence_lemmas.csv',
+    '../../data/train_metrics_sequence_stems.csv',
+    '../../data/train_metrics_sequence_tokens.csv'
+]
 
 TARGET = 'is_duplicate'
-qid1,  qid2 = 'qid1',  'qid2'
+qid1, qid2 = 'qid1', 'qid2'
 
 question1, question2 = 'question1', 'question2'
-lemmas_q1, lemmas_q2 ='lemmas_q1', 'lemmas_q2'
-stems_q1,stems_q2='stems_q1','stems_q2'
-tokens_q1,tokens_q2='tokens_q1','tokens_q2'
+lemmas_q1, lemmas_q2 = 'lemmas_q1', 'lemmas_q2'
+stems_q1, stems_q2 = 'stems_q1', 'stems_q2'
+tokens_q1, tokens_q2 = 'tokens_q1', 'tokens_q2'
+
 
 def normalize_str(s):
     try:
@@ -45,38 +58,68 @@ def normalize_str(s):
 
 def normalize_and_store_df(df, fp):
     df = df[['question1', 'question2']]
-    df['norm1']=df['question1'].apply(normalize_str)
-    df['norm2']=df['question2'].apply(normalize_str)
-    df[['norm1','norm2']].to_csv(fp, index_label='id')
+    df['norm1'] = df['question1'].apply(normalize_str)
+    df['norm2'] = df['question2'].apply(normalize_str)
+    df[['norm1', 'norm2']].to_csv(fp, index_label='id')
 
 
 def load_train():
     return pd.read_csv(fp_train, index_col='id')
 
+
+def load__train_metrics():
+    dfs = [pd.read_csv(fp, index_col='id') for fp in METRICS_FP]
+    return pd.concat(dfs, axis=1)
+
+
 def load_train_all():
-    return pd.concat([load_train(), load_train_lemmas(), load_train_stems(), load_train_tokens()], axis=1)
+    return pd.concat([
+        load_train(),
+        load_train_lemmas(),
+        load_train_stems(),
+        load_train_tokens(),
+        load_train_lengths(),
+        load_train_common_words(),
+        load__train_metrics()
+    ], axis=1)
+
 
 def load_train_test():
     return pd.read_csv(fp_train, index_col='id'), pd.read_csv(fp_test, index_col='test_id')
 
+
 def load_train_lemmas():
-    df= pd.read_csv(lemmas_train_fp, index_col='id')
+    df = pd.read_csv(lemmas_train_fp, index_col='id')
     df = df.fillna('')
     return df
+
 
 def load_train_tokens():
-    df= pd.read_csv(tokens_train_fp, index_col='id')
+    df = pd.read_csv(tokens_train_fp, index_col='id')
     df = df.fillna('')
     return df
 
+
 def load_train_stems():
-    df= pd.read_csv(stems_train_fp, index_col='id')
+    df = pd.read_csv(stems_train_fp, index_col='id')
     df = df[['question1_porter', 'question2_porter']]
-    df = df.rename(columns={'question1_porter':'stems_q1', 'question2_porter':'stems_q2'})
+    df = df.rename(columns={'question1_porter': 'stems_q1', 'question2_porter': 'stems_q2'})
     df = df.fillna('')
     return df
+
+
+def load_train_common_words():
+    df = pd.read_csv(common_words_train_fp, index_col='id')
+    return df
+
+
+def load_train_lengths():
+    df = pd.read_csv(length_train_fp, index_col='id')
+    return df
+
 
 def load_train_normalized_train():
     return pd.read_csv(normalized_train_fp, index_col='id')
+
 
 df = load_train_all()
