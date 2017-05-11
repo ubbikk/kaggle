50dd1623e59cdfe247c0ad79d57032736e7a7cf1
ubik <dd.petrovskiy@gmail.com>
2017-05-11 19:53:48 +0300

    working

--- quora/generate_stem_my.py
+++ quora/generate_stem_my.py
@@ -6,6 +6,7 @@ from nltk.stem.snowball import SnowballStemmer
 seed = 1024
 np.random.seed(seed)
 path = '../../data/'
+import os
 
 train = pd.read_csv(path+"train.csv")
 test = pd.read_csv(path+"test.csv")
@@ -27,6 +28,6 @@ test['question1_porter'] = test['question1'].astype(str).apply(lambda x:stem_str
 train['question2_porter'] = train['question2'].astype(str).apply(lambda x:stem_str(x.lower(),porter))
 test['question2_porter'] = test['question2'].astype(str).apply(lambda x:stem_str(x.lower(),porter))
 
-train.to_csv(path+'train_porter.csv')
-test.to_csv(path+'test_porter.csv')
+train.to_csv(os.path.join(path, 'nlp', 'stems_train.csv'), index_label='id')
+test.to_csv(os.path.join(path, 'nlp', 'stems_test.csv'), index_label='test_id')
 
