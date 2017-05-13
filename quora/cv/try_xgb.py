ed881f3a5bc91e91dd7fff9c91b56c29d17d46dd
ubik <dd.petrovskiy@gmail.com>
2017-05-14 00:45:03 +0300

    working

--- quora/cv/try_xgb.py
+++ quora/cv/try_xgb.py
@@ -1,8 +1,13 @@
+from time import time
+
 import pandas as pd
 import numpy as np
 import seaborn as sns
 import re
 import os
+
+from sklearn.metrics import log_loss
+from sklearn.model_selection import StratifiedKFold
 
 sns.set(color_codes=True)
 sns.set(style="whitegrid", color_codes=True)
@@ -11,7 +16,7 @@ pd.set_option('display.width', 5000)
 pd.set_option('display.max_rows', 5000)
 pd.set_option('display.max_colwidth', 100)
 
-data_folder = '../../data/'
+data_folder = '../../../data/'
 
 fp_train = data_folder + 'train.csv'
 fp_test = data_folder + 'test.csv'
@@ -86,6 +91,21 @@ def load_train_all():
         load_train_tfidf()
     ], axis=1)
 
+def load_train_all_xgb():
+    train_df = pd.concat([
+        load_train(),
+        load_train_lengths(),
+        load_train_common_words(),
+        load__train_metrics(),
+        load_train_tfidf()
+    ], axis=1)
+
+    cols_to_del = [qid1, qid2, question1, question2]
+    for col in cols_to_del:
+        del train_df[col]
+
+    return train_df
+
 
 def load_train_test():
     return pd.read_csv(fp_train, index_col='id'), pd.read_csv(fp_test, index_col='test_id')
@@ -155,5 +175,152 @@ def load_train_lengths():
     return df
 
 
+############################################################3
+############################################################3
+############################################################3
 
-df = load_train_all()
+
+import pandas as pd
+import numpy as np
+
+TARGET = 'is_duplicate'
+
+INDEX_PREFIX= 100000000
+#old
+{'pos': 0.369197853026293,
+ 'neg': 0.630802146973707}
+
+
+#new
+r1 = 0.174264424749
+r0 = 0.825754788586
+
+""""
+p_old/(1+delta) = p_new
+
+delta = (p_old/p_new)-1 = 1.1186071314214785
+l = delta*N = 452241
+"""
+
+L= 452241
+
+def explore_target_ratio(df):
+    return {
+        'pos':1.0*len(df[df[TARGET]==1])/len(df),
+        'neg':1.0*len(df[df[TARGET]==0])/len(df)
+    }
+
+def shuffle_df(df, random_state):
+    np.random.seed(random_state)
+    return df.iloc[np.random.permutation(len(df))]
+
+def oversample_df(df, l, random_state):
+    df_pos = df[df[TARGET]==1]
+    df_neg = df[df[TARGET]==0]
+
+    df_neg_sampl = df_neg.sample(l, random_state=random_state, replace=True)
+
+    df=pd.concat([df_pos, df_neg, df_neg_sampl])
+    df = shuffle_df(df, random_state)
+
+    return df
+
+def oversample(train_df, test_df, random_state=42):
+    return oversample_df(train_df, L, random_state), oversample_df(test_df, L, random_state)
+
+
+
+############################################################3
+############################################################3
+############################################################3
+import xgboost as xgb
+import matplotlib.pyplot as plt
+
+def plot_errors(imp):
+    train_runs= [x['train'] for x in imp]
+    test_runs= [x['test'] for x in imp]
+
+    sz=len(train_runs[0])
+    x_axis=range(sz)
+    y_train = [np.mean([x[j] for x in train_runs]) for j in x_axis]
+    y_test = [np.mean([x[j] for x in test_runs]) for j in x_axis]
+
+    fig, ax = plt.subplots()
+    ax.plot(x_axis, y_train, label='train')
+    ax.plot(x_axis, y_test, label='test')
+    ax.legend()
+    plt.show()
+
+def xgboost_per_tree_results(estimator):
+    results_on_test = estimator.evals_result()['validation_1']['logloss']
+    results_on_train = estimator.evals_result()['validation_0']['logloss']
+    return {
+        'train': results_on_train,
+        'test': results_on_test
+    }
+
+def out_loss(loss):
+    print '====================================='
+    print '====================================='
+    print '====================================='
+    print loss
+    print '====================================='
+    print '====================================='
+    print '====================================='
+
+def perform_xgb_cv():
+    df = load_train_all_xgb()
+    print df.columns.values
+    folds =5
+    seed = 42
+    skf = StratifiedKFold(n_splits=folds, shuffle=True, random_state=seed)
+    losses = []
+    stats = []
+    imp = []
+    for big_ind, small_ind in skf.split(np.zeros(len(df)), df[TARGET]):
+        big = df.iloc[big_ind]
+        small = df.iloc[small_ind]
+        big, small = oversample(big, small, seed)
+
+        train_target = big[TARGET]
+        del big[TARGET]
+        train_arr = big
+
+        test_target = small[TARGET]
+        del small[TARGET]
+        test_arr = small
+
+        estimator = xgb.XGBClassifier(n_estimators=10000,
+                                      subsample=0.8,
+                                      colsample_bytree=0.8,
+                                      max_depth=5)
+        eval_set = [(test_arr, test_target), (train_arr, train_target)]
+        estimator.fit(
+            train_arr, train_target,
+            eval_set=eval_set,
+            eval_metric='logloss',
+            verbose=True,
+            early_stopping_rounds=200
+        )
+
+        proba = estimator.predict_proba(test_arr)
+        loss = log_loss(test_target, proba)
+        out_loss(loss)
+        losses.append(loss)
+        stats.append(xgboost_per_tree_results(estimator))
+        imp.append(estimator.feature_importances_)
+        # xgb.plot_importance(estimator)
+        # plot_errors(stats)
+
+
+    print out_loss('avg = {}'.format(np.mean(losses)))
+
+
+perform_xgb_cv()
+
+
+
+
+
+
+
